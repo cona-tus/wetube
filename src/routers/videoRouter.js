9fa472fa0ec818f2012efcdb92dba15bd1491660
cona-tus <cam_panella@naver.com>
2021-12-13 20:41:17 +0900

    templates

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,17 +1,11 @@
 import express from "express";
-import { see } from "../controllers/userController";
-import {
-  watch,
-  edit,
-  upload,
-  deleteVideo,
-} from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
