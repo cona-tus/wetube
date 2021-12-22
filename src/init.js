6875db44fabb2c4bf3473e08b15f5a87915170bb
cona-tus <cam_panella@naver.com>
2021-12-22 12:35:59 +0900

    Video Edit base

--- src/init.js
+++ src/init.js
@@ -1,10 +1,10 @@
-import "./db.js";
+import "./db";
 import "./models/Video";
 import app from "./server";
 
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
